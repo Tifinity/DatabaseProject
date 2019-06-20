b07066bec32be058dacc99445e2b1f69ba59578a
TempterCyn <48970853+TempterCyn@users.noreply.github.com>
2019-06-20 23:10:13 +0800

    那么， l

---
+++ ReturnBook.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <cstring>
+#include<string>
+#include<iostream>
+#include"mysql.h"
+using namespace std;
+class ReturnBook {
+public:
+	ReturnBook(MYSQL m_);
+	void setValue(string cusName_, string bookName_, string sdate_, string rdate_);
+	bool check();
+	void deleteSellTuple(char* src);
+	void updateStock();
+	void printReturnList();
+	void addReturnInfo();
+	void Return();
+private:
+	MYSQL m;
+	string cusName;
+	string bookName;
+	string sdate;
+	string rdate;
+	char bid[20];
+};
