146cf2931e6b317e125ad89fe415594ffe230b7e
Tifinity <1139390530@qq.com>
2019-06-20 23:25:07 +0800

    22222

--- sell.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-//销售记录
-class Sell {
-public:
-	int get_total_sales(int month);//获取某月的销售总额
-	int get_total_number(int month);//获取某月的销售总量
-	void get_top_ten_books(int month);//输出某月销量前10的书
-};
